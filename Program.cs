fecd7a3cf843a6b750cbe4acf3af98d444e34006
JacksonSmithStudent <jasm076@student.oaisd.org>
2024-11-04 15:00:06 +0000

    added write stuff

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Write1ThroughN_While(-1);
+        Write1ThroughN_While(5);
+        Write1ThroughN_While(0);
+        Write1ThroughN_For(-1);
+        Write1ThroughN_For(5);
+        Write1ThroughN_For(0);
+        WriteNThrough1_While(-1);
+        WriteNThrough1_While(5);
+        WriteNThrough1_While(0);
+        
+   
